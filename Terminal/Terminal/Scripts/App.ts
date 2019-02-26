f616b39825f65f47f0308e44553da4cdeb1b6985
Benoit Robin <clefweb@outlook.com>
2019-02-26 02:41:21 -0500

    Initial commit. Desing test phase

---
+++ Terminal/Terminal/Scripts/App.ts
@@ -0,0 +1,37 @@
+﻿
+$(function () {
+	var gApp = new App();
+});
+class App {
+	term: JQueryTerminal;
+	private cancellExecution(): boolean {
+
+		return false;
+	}
+	constructor() {
+
+		this.term = $('#HomeTerm').terminal((command) => {
+			var term = this.term;
+			var cmd = $.terminal.parse_command(command);
+			if (cmd.name == 'progress') {
+				
+			}
+		}, {
+				greetings: 'JavaScript Interpreter',
+				name: 'js_demo',
+				height: 200,
+				prompt: '$> ',
+				keydown:  (e) => {
+					
+					if (e.which == 68 && e.ctrlKey) { // CTRL+D
+						return this.cancellExecution();
+						}
+						return false;
+					
+				}
+			});
+		
+		
+	}
+	
+}
